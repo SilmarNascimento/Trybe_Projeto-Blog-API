e2cf26962d7b57cdba490b848a16d3703e896fb4
Shanloo <silmarfnascimento@gmail.com>
2023-07-13 18:03:15 -0300

    feat req11: implementa a model PostCategory.js e suas associações com a model category.js e BlogPost.js

---
+++ src/models/PostCategory.js
@@ -0,0 +1,48 @@
+/**
+ * 
+ * @param {import('sequelize').Sequelize} sequelize 
+ * @param {import('sequelize').DataTypes} DataTypes 
+ */
+
+const PostCategory = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    categoryId: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    }
+  }, {
+    tableName: 'post_categories',
+    timestamps: false,
+    underscored: true,
+  });
+
+  /**
+   * 
+   * @param {import('../models')} models 
+   */
+  PostCategory.associate = (models) => {
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'posts',
+      foreignKey: 'category_id',
+      otherKey: 'post_id',
+      through: PostCategory,
+    });
+
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      foreignKey: 'post_id',
+      otherKey: 'category_id',
+      through: PostCategory,
+    });
+  }
+
+  return PostCategory;
+};
+
+module.exports = PostCategory;
